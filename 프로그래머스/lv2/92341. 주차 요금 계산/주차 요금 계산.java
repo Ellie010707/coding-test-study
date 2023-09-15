9501a6377e6851dfe23277748c0b2f884d1c1caf
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2023-09-15 18:25:40 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.94 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -4,7 +4,7 @@ import java.util.TreeMap;
 
 class Solution {
     
-    public final int LASTTIME = 1439;
+    public final int LASTTIME = 1439;   //23:59
     
     public int[] solution(int[] fees, String[] records) {
        
@@ -18,7 +18,7 @@ class Solution {
         for (String record : records) {
             String[] split_record = record.split(" ");
             String[] split_time = split_record[0].split(":");
-            int tmp_time = (split_record[2].equals("IN") ? -1 : 1) * ((Integer.parseInt(split_time[0])*60) + Integer.parseInt(split_time[1]));
+            int tmp_time = (split_record[2].equals("IN") ? -1 : 1) * ((Integer.parseInt(split_time[0])*60) + Integer.parseInt(split_time[1])); // 현재 time 계산 in 이면 빼고 out 이면 더함
             String car = split_record[1];
             map.put(car, map.getOrDefault(car, 0) + tmp_time);
         }
@@ -27,9 +27,9 @@ class Solution {
         int[] answer = new int[map.size()];
         for (String key : map.keySet()) {
             int total_time = map.get(key);
-            total_time = total_time <= 0 ? total_time + LASTTIME : total_time;
-            double time = total_time - default_time < 0 ? 0 : total_time - default_time;
-            int price = default_fee + (int)Math.ceil(time/unit_time) * unit_fee;
+            total_time = total_time <= 0 ? total_time + LASTTIME : total_time; //전체 시간이 마이너스 == 출차 계산 안됨 -> 23:59 더해줌
+            double time = total_time - default_time < 0 ? 0 : total_time - default_time; //기본 시간보다 적으면 0으로 변경(추가금 X)
+            int price = default_fee + (int)Math.ceil(time/unit_time) * unit_fee; //금액 계산
             answer[i++] = price;
         }
         
