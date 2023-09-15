523800c3be7d1da7e617ab749438836c8c6b8c9c
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2023-09-15 22:43:16 +0900

    [level 2] Title: 순위 검색, Time: 2605.87 ms, Memory: 306 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] solution(String[] info, String[] query) {
+        final Map<String, int[]> infos = new HashMap<>();
+        for (final String s : info) {
+            addAllCaseInfo(s, infos);
+        }
+
+        final int[] answer = new int[query.length];
+        for (int index = 0; index < answer.length; index++) {
+            final String[] split = query[index].replaceAll(" and ", "").split(" ");
+            final int[] infoScore = infos.get(split[0]);
+            if(infoScore == null) {
+                answer[index] = 0;
+            }
+
+            if(infoScore != null) {
+                for (int i = Integer.parseInt(split[1]); i < infoScore.length; i++) {
+                    answer[index] += infoScore[i];
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private void addAllCaseInfo(final String s, final Map<String, int[]> infos) {
+        final String[] split = s.split(" ");
+        for (String language : new String[]{split[0], "-"}) {
+            for (String job : new String[]{split[1], "-"}) {
+                for (String career : new String[]{split[2], "-"}) {
+                    for (String food : new String[]{split[3], "-"}) {
+                        final String info = language + job + career + food;
+                        final int score = Integer.parseInt(split[4]);
+                        if(infos.containsKey(info)) {
+                            infos.get(info)[score]++;
+                        } else {
+                            final int[] arr = new int[100001];
+                            arr[score]++;
+                            infos.put(info, arr);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
