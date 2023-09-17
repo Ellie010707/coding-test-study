f0b55d14714ff1261e2a138ff18064fe85d63185
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2023-09-17 17:04:57 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.62 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -28,7 +28,7 @@ class Solution {
         for (String key : map.keySet()) {
             int total_time = map.get(key);
             total_time = total_time <= 0 ? total_time + LASTTIME : total_time; //전체 시간이 마이너스 == 출차 계산 안됨 -> 23:59 더해줌
-            double time = total_time - default_time < 0 ? 0 : total_time - default_time; //기본 시간보다 적으면 0으로 변경(추가금 X)
+            double time = total_time - default_time <= 0 ? 0 : total_time - default_time; //기본 시간보다 적으면 0으로 변경(추가금 X)
             int price = default_fee + (int)Math.ceil(time/unit_time) * unit_fee; //금액 계산
             answer[i++] = price;
         }
