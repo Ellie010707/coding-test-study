d880b79af12905fba3b4c9d5472fe3bc5127fb99
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2023-09-17 17:57:36 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.08 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -8,8 +8,7 @@ class Solution {
         for (String kNum: kNums) {
             if(kNum.equals("") || kNum.equals("1")) continue;
             
-            long decimalNum = Long.parseLong(kNum); //10진법으로 변환
-            //int decimalNum = Integer.parseInt(kNum, k); //처음에 이렇게 변환했는데 틀림
+            long decimalNum = Long.parseLong(kNum);
             
             isPrime = true;
             for (int i = 2; i <= (int)Math.sqrt(decimalNum); i++){
