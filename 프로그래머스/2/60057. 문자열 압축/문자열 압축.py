41e09751131048c191761219f39b325ab4b200c7
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2023-10-14 01:43:05 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,24 +1,20 @@
 def solution(s):
-    answer = len(s)  # 문자열을 1개 단위로 자를 경우의 길이로 초기화
+    answer = len(s)
 
-    # 문자열을 자를 단위를 1부터 (문자열 길이 // 2)까지 반복
-    for step in range(1, len(s) // 2 + 1):
-        compressed = ""
-        prev = s[0:step]  # 이전에 비교한 문자열
-        count = 1  # 반복 횟수
 
-        # 단위 당 문자열 비교
-        for j in range(step, len(s), step):
-            if prev == s[j:j + step]:
-                count += 1
-            else:
-                compressed += (str(count) + prev) if count > 1 else prev
-                prev = s[j:j + step]
-                count = 1
-
-        compressed += (str(count) + prev) if count > 1 else prev
-
-        # 현재 단위로 압축한 문자열의 길이가 더 짧다면 업데이트
-        answer = min(answer, len(compressed))
-
+    for i in range(1, len(s) // 2 + 1): # 압축 크기, 최대 압축 크기만큼 반복
+        comp = ""       # 압축한 문자열
+        prev = s[:i]    # 첫 데이터
+        cnt = 1         # 반복 횟수
+        
+        for j in range(i, len(s) + 1, i): # 두 번째 데이터 부터 마지막 까지
+            if prev == s[j:j + i]:  # prev가 다음 데이터랑 같을 경우
+                cnt += 1
+            else:   # 다르면
+                comp += str(cnt) + prev if cnt > 1 else prev   # 압축 문자열 생성 
+                prev = s[j:j + i]
+                cnt = 1
+        comp += str(cnt) + prev if cnt > 1 else prev 
+        answer = min(len(comp), answer) # 더 짧으면 갱신
+        
     return answer
