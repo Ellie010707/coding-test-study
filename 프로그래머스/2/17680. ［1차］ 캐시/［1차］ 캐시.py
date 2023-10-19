5f34c2287329db4e1839c32731e86c8f7845825b
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2023-10-19 18:41:16 +0900

    [level 2] Title: [1차] 캐시, Time: 0.67 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,13 +1,13 @@
 def solution(cacheSize, cities):
-    answer = 0 # 실행 시간
-    
-    cities = [c.lower() for c in cities]
+    answer = 0  # 실행 시간
+
     cache = []
-    
+
     if cacheSize == 0:
-        return 5*len(cities)
-    
+        return 5 * len(cities)
+
     for city in cities:
+        city = city.lower()
         if city in cache:
             answer += 1
             cache.append(cache.pop(cache.index(city)))
@@ -16,5 +16,5 @@ def solution(cacheSize, cities):
             if len(cache) >= cacheSize:
                 cache.pop(0)
             cache.append(city)
-    
+
     return answer
