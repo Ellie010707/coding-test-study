191c5b06ba013ae5560f7d76b6fc361f2d6e05f7
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2024-01-10 18:44:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.65 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,6 @@
-from collections import Counter
+import collections
 
 def solution(participant, completion):
-
-    count = Counter(completion)
-    for p in participant: 
-        if count[p] <= 0:
-            return p
-        count[p] -= 1
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
         
