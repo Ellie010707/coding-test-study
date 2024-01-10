eced4cb33410caa320177d0aa56ef4b1269206bf
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2024-01-10 18:57:22 +0900

    [level 1] Title: 폰켓몬, Time: 0.84 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -3,4 +3,4 @@ from collections import Counter
 def solution(nums):
     max_i = len(nums)//2
     count = Counter(nums)
-    return len(count.keys()) if len(count.keys()) <= max_i else max_i
+    return min(max_i, len(count.keys()))
