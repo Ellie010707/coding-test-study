15ba0b5369fd4edb9b445ef440b2c4b1a83e9546
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2024-01-17 00:32:33 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,21 +1,11 @@
 def solution(word):
-    d = ['', 'A', 'E', 'I', 'O', 'U']
-    cnt = 0
-    for i1 in d:
-        if not i1: continue        
-        for i2 in d:
-            if i2 and not i1: break
-            for i3 in d:
-                if i3 and not i2: break
-                for i4 in d:
-                    if i4 and not i3: break
-                    for i5 in d:
-                        if i5 and not i4: break
-                        cnt += 1
-                        if i1+i2+i3+i4+i5 == word:
-                            print(i1+i2+i3+i4+i5)
-                            return cnt
-                        
-                        
-                        
+    answer = 0
+    dic = ['A', 'E', 'I', 'O', 'U']
+    li = [5**i for i in range(len(dic))]
+    
+    for i in range(len(word)-1,-1,-1):
+        idx = dic.index(word[i])
+        for j in range(5-i):
+            answer += li[j]*idx
+        answer+=1
     return answer
