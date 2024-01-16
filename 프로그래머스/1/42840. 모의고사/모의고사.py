b99bbf9ac5f711e51e917a40fc2ec2505a2ed3d2
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2024-01-16 23:39:54 +0900

    [level 1] Title: 모의고사, Time: 3.74 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,14 @@
+def solution(answers):
+    answer = [0,0,0]
+    
+    length = len(answers)
+    person1 = [1, 2, 3, 4, 5] * (length // 5 + 1)
+    person2 = [2, 1, 2, 3, 2, 4, 2, 5] * (length // 8 + 1)
+    person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * (length // 10 + 1)
+    
+    for i, a in enumerate(answers):
+        if person1[i] == a: answer[0] += 1
+        if person2[i] == a: answer[1] += 1
+        if person3[i] == a: answer[2] += 1
+    
+    return [i+1 for i, a in enumerate(answer) if a == max(answer)]
