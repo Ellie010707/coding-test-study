5095cf7ff32b4bfa86f1f5b66878f27e5aae647a
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2024-01-16 18:35:03 +0900

    [level Lv.2] Title: 타겟 넘버, Time: 26.46 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,20 @@
+# 6:29
+import copy
+answer = 0
+def solution(numbers, target):
+    def dfs(now, numbers, target):
+        global answer
+        if len(numbers) == 0:
+            if now == target:
+                answer += 1
+                return
+            else: return
+        
+        tmp_numbers = copy.deepcopy(numbers)
+        tmp = tmp_numbers.pop(0)
+        
+        dfs(now + tmp, tmp_numbers, target)
+        dfs(now - tmp, tmp_numbers, target)
+        
+    dfs(0, numbers, target)
+    return answer
