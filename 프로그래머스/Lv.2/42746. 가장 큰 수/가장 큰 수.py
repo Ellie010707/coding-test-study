c9c6214ddbf491629340cad372e21964282ac270
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2024-01-16 17:13:52 +0900

    [level Lv.2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/Lv.2/42746. 가장 큰 수/가장 큰 수.py
@@ -3,6 +3,7 @@ def solution(numbers):
     if len(numbers) == 1: return str(numbers[0])
     
     numbers = list(map(str, numbers))
-    numbers.sort(key = lambda x: x*4, reverse = True)
+    numbers.sort(key = lambda x: x*3, reverse = True)
     
     return str(int("".join(numbers)))
+
