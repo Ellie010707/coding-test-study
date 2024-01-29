180c4065cce031d2627d8a0800a88e6651e26367
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2024-01-29 23:00:17 +0900

    [Silver II] Title: DFS와 BFS, Time: 560 ms, Memory: 39020 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,47 @@
+from queue import deque
+import copy
+
+N, M, V = map(int, input().split())
+
+tmp = [[int(x) for x in input().split()] for _ in range(M)]
+
+def dfs(x):
+    global visited, datas
+    visited[x] = 1
+    print(x, end=" ")
+
+    if visited.count(0) == 1 or not datas[x]:
+        return 
+    
+    for i in datas[x]:
+        if visited[i] == 0:
+            dfs(i)
+    return
+
+def bfs(x):
+    global visited, datas
+    queue = deque([x])
+
+    while queue:
+        x = queue.popleft()
+        visited[x] = 1
+        print(x, end=" ")
+
+        for i in datas[x]:
+            if visited[i] == 0 and i not in queue:
+                queue.append(i)
+    return
+
+datas = [[] for _ in range(N+1)]
+for i, j in tmp: 
+    datas[i].append(j)
+    datas[j].append(i)
+for data in datas: data.sort()
+
+visited = [0] * (N + 1)
+dfs(V)
+print()
+
+visited = [0] * (N + 1)
+bfs(V)
+print()
