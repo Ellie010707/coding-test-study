1d3eaa8af5c0d1cb6fcd9b8c872f93b904d88984
codinglim :3 <41159837+Ellie010707@users.noreply.github.com>
2024-01-31 23:11:50 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -2,11 +2,15 @@
 -- 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물
 -- ID 순으로 조회하는 SQL문을 작성해주세요.
 
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_OUTS 
-WHERE ANIMAL_ID not in (SELECT O.ANIMAL_ID
-                        FROM ANIMAL_OUTS as O
-                        JOIN ANIMAL_INS as I
-                        ON O.ANIMAL_ID = I.ANIMAL_ID)
-
-
+# SELECT ANIMAL_ID, NAME
+# FROM ANIMAL_OUTS 
+# WHERE ANIMAL_ID not in (SELECT O.ANIMAL_ID
+#                         FROM ANIMAL_OUTS as O
+#                         JOIN ANIMAL_INS as I
+#                         ON O.ANIMAL_ID = I.ANIMAL_ID)
+                        
+SELECT O.ANIMAL_ID, O.NAME
+FROM ANIMAL_OUTS as O
+LEFT JOIN ANIMAL_INS as I
+ON O.ANIMAL_ID = I.ANIMAL_ID
+WHERE I.ANIMAL_ID is NULL
